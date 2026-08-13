2b268913c729d58bf56d13b752f07b1f3121df93
LeeKiWoong <littlemanner1@gmail.com>
2026-08-13 11:51:48 +0900

    [feat] Component Extension 확장 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### Component Extension
    - 로깅을 할 때 어떤 오브젝트에서 로그를 발행하는지 확인하기 쉽게 하기 위해서 확장 메서드 형식으로 구현하였음
    
    - 로그 발행 시 "(하이어라키 상의 게임 오브젝트 이름).(컴포넌트의 타입 이름) : 로그" 식으로 출력되게 하였으며, 콘솔 창에서 로그 클릭 시, 하이어라키에서 해당 게임 오브젝트가 하이라이트되어 유니티에서 확인이 쉽게 구현함

---
+++ Assets/Scripts/Extension/Component Extension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class ComponentExtension
+{
+    public static void Log(this Component component, string text)
+    {
+        Debug.Log($"{component.gameObject.name}.{component.GetType().Name} : {text}", component);
+    }
+
+    public static void LogWarning(this Component component, string text)
+    {
+        Debug.LogWarning($"{component.gameObject.name}.{component.GetType().Name} : {text}", component);
+    }
+
+    public static void LogError(this Component component, string text)
+    {
+        Debug.LogError($"{component.gameObject.name}.{component.GetType().Name} : {text}", component);
+    }
+}
