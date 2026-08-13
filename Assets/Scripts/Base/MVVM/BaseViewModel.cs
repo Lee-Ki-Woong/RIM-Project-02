88472b5be67a7e3aa9d6e3d5c51774576c59e734
LeeKiWoong <littlemanner1@gmail.com>
2026-08-13 11:55:02 +0900

    [feat] BaseViewModel, BaseViewModel<T> 추상 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### BaseViewModel
    - MVVM 아키텍처에서 사용할 ViewModel의 추상 클래스로 Model의 변경을 감지해 View로 중계하는 역할을 함
    
    - OnPropertyChanged는 Model의 PropertyChanged 이벤트와 연결되는 핸들러로, Model에서 온 알림을 받아 OnPropertyChangedForView 이벤트로 중계하는 역할을 하며, View에서 이 이벤트를 구독하여 갱신을 하게 만들었음
    
    - Model의 PropertyChanged 이벤트를 구독하기 때문에, 사용이 끝난 시점에 구독을 해제해 메모리 누수를 방지할 수 있도록 IDisposable 인터페이스를 구현하였음
    
    - PropertyChangedOnInit과 Dispose를 abstract로 선언하여 파생 클래스가 구현하도록 강제하였음
    
    #### BaseViewModel<T> where T : BaseModel
    - BaseViewModel을 상속받은 파생 클래스로 실제로 사용하기 위한 ViewModel 기반 클래스
    
    - 제네릭을 사용해 바인딩 받는 model 클래스를 BaseModel을 상속받은 파생클래스로 제한을 두었음
    
    - 생성자에서 model을 주입받아 _model에 보관하고, _model.PropertyChanged 이벤트를 구독함
    
    - PropertyChangedOnInit을 오버라이드하여 _model.PropertyChangedOnInit()을 호출하게 만듬으로써, 초기화 시점의 일괄 갱신을 Model 클래스에 위임함 (=> Model의 초기화는 Model이 한다는 확실한 역할 분리)
    
    - Dispose를 오버라이드하여 생성자에서 구독한 _model.PropertyChanged 이벤트의 구독을 해제하여, ViewModel 소멸시 Model의 이벤트 참조가 남지않도록 하였음

---
+++ Assets/Scripts/Base/MVVM/BaseViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+
+public abstract class BaseViewModel : IDisposable
+{
+    public event Action<string> OnPropertyChangedForView;
+
+    protected virtual void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEvent)
+    {
+        OnPropertyChangedForView?.Invoke(propertyChangedEvent.PropertyName);
+    }
+
+    public abstract void PropertyChangedOnInit();
+
+    public abstract void Dispose();
+}
+
+public abstract class BaseViewModel<T> : BaseViewModel where T : BaseModel
+{
+    protected readonly T _model;
+
+    public BaseViewModel(T model)
+    {
+        _model = model;
+        _model.PropertyChanged += OnPropertyChanged;
+    }
+
+    public override void PropertyChangedOnInit()
+    {
+        _model.PropertyChangedOnInit();
+    }
+
+    public override void Dispose()
+    {
+        _model.PropertyChanged -= OnPropertyChanged;
+    }
+}
