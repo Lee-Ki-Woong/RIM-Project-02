f16607082323dde09ef66a49bf6f5077d69d3f69
LeeKiWoong <littlemanner1@gmail.com>
2026-08-13 15:44:23 +0900

    [feat] UIDataManager 및 관련 임시 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### UIDataBase
    - Json으로 데이터드리븐한 데이터들의 부모 클래스로, 모든 UI 데이터가 소지하는 Id 필드를 가지고 있음
    
    #### UIDataManager
    - BaseManager를 상속받아, 언어별 Json 데이터를 로드 및 보관하는 순수 C# 기반 매니저 클래스
    
    - InitAction 시점에 데이터를 전부 불러오며, 그 후 ReloadAllData로 런타임에 재로드를 할 수 있도록 구현하였음
    
    - LoadData<T>는 언어에 맞는 JSON(TextAsset)을 ResourceManager로 로드한 뒤, JsonUtility로 파싱하여 string(Id)를 키값으로 가지는 딕셔너리로 반환함
    
    - 중복된 Id의 경우 경고 로그로 어떤 Id가 중복인지 알려준 후 continue를 하도록 하였음
    
    - 리소스 누락, 파싱 실패, 예외 등등 어떤 문제가 발생할 경우 null값이 아니라 빈 딕셔너리를 반환하여 NullReference 오류를 방지함
    
    - 파싱이 끝난 TextAsset의 경우 finally에서 ResourceManager의 UnloadAsset을 호출해 해제하여, 메모리에서 해제하도록 구현하였음
    
    #### GameManager
    - 지금은 임시로 구현하였으며, 내부에 GameCurrentLanguage로 지금 게임이 어떤 언어를 사용하는지를 열거형으로 가지고 있음
    
    #### GameEnums
    - 게임에 필요한 열거형을 가지고 있으며, 현재 Language라는 열거형을 소지 중
    
    #### LanguageUtil
    - 언어에 관련된 Language 열거형을 string값으로 바꾸거나, string값을 Language 열거형으로 바꾸는 처리를 하고 있음

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class GameManager : BaseMonoManager<GameManager>
+{
+    public Language GameCurrentLanguage { get; private set; }
+}
---
+++ Assets/Scripts/Util/LanguageUtil.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public static class LanguageUtil
+{
+    public static string GetLanguage(Language language)
+    {
+        switch (language)
+        {
+            case Language.English:
+                {
+                    return "English";
+                }
+            case Language.Korean:
+                {
+                    return "Korean";
+                }
+            default:
+                {
+                    Debug.LogWarning($"GameUtil : 잘못된 [Enum : Language] 값이 들어왔습니다!! 값 : {language}");
+                    return "Korean";
+                }
+        }
+    }
+
+    public static Language GetLanguage(string language)
+    {
+        switch (language)
+        {
+            case "English":
+                {
+                    return Language.English;
+                }
+            case "Korean":
+                {
+                    return Language.Korean;
+                }
+            default:
+                {
+                    Debug.LogWarning($"GameUtil : 잘못된 [string : Language] 값이 들어왔습니다!! 값 : {language}");
+                    return Language.Korean;
+                }
+        }
+    }
+}
