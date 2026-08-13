2c1d9dc53bba603ff465ae7db4663bc046e8e738
LeeKiWoong <littlemanner1@gmail.com>
2026-08-13 11:50:09 +0900

    [feat] BaseMonoManager 추상 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### BaseMonoManager
    - 싱글턴 패턴을 사용해 구현한 매니저 클래스로 Unity의 MonoBehaviour를 상속 받음
    
    - Awake 내부에서 작업이 필요할 경우 AwakeAction(추후 메서드 이름 변경 가능)을 오버라이딩해서 사용하면 됨

---
+++ Assets/Scripts/Base/Manager/BaseMonoManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BaseMonoManager<T> : MonoBehaviour where T : BaseMonoManager<T>
+{
+    public static T Instance { get; private set; }
+
+    private void Awake()
+    {
+        InitSingleTon();
+        AwakeAction();
+    }
+
+    private void InitSingleTon()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void AwakeAction()
+    {
+        // Awake에서 추가 작업이 필요할 경우 여기에 작성
+    }
+}
