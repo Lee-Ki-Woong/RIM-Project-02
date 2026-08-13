1d7d05fe4cca94fe45952555176dc8b9ebb0787d
LeeKiWoong <littlemanner1@gmail.com>
2026-08-13 11:52:45 +0900

    [feat] MonoBehaviour 확장 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### MonoBehaviour Extension
    - 모노비헤이비어에서 게임오브젝트를 활성화/비활성화 할 때 편하게 호출하기 위해서 ActiveTrue, ActiveFalse 메서드를 추가해둠

---
+++ Assets/Scripts/Extension/MonoBehaviour Extension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class MonoBehaviourExtension
+{
+    public static void ActiveTrue(this MonoBehaviour monoBehaviour)
+    {
+        monoBehaviour.gameObject.SetActive(true);
+    }
+
+    public static void ActiveFalse(this MonoBehaviour monoBehaviour)
+    {
+        monoBehaviour.gameObject.SetActive(false);
+    }
+}
