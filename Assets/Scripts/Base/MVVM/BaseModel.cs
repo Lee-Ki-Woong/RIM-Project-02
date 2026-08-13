991c97aa7e9aa270bc180ed2bf5dbd4101cbb0ee
LeeKiWoong <littlemanner1@gmail.com>
2026-08-13 11:54:02 +0900

    [feat] BaseModel 추상 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### BaseModel
    - MVVM에서 사용할 Model의 추상 클래스
    
    - INotifyPropertyChanged 인터페이스를 구현하였음. 파생 클래스의 프로퍼티 setter에서 OnPropertyChanged 메서드를 호출하면 PropertyChanged 이벤트가 발생하며, 이 이벤트를 구독한 ViewModel/View가 해당 변경을 감지해 자동으로 갱신되도록 하였음
    
    - OnPropertyChanged는 protected로 선언하여 파생 클래스에서만 알림을 발생시킬 수 있도록 제한함
    
    - PropertyChangedOnInit를 abstract로 선언하여 파생 클래스가 반드시 구현하도록 강제하였으며, 초기화 시점에 갱신이 필요한 프로퍼티들에 대해 OnPropertyChanged를 호출하는 로직을 이 메서드에 모아두면, 초기화 과정에서 View를 일괄 갱신할 수 있게 하였음
    
    - CallerMemberName 어트리뷰트를 사용해서 프로퍼티의 Setter에서 nameof(프로퍼티명)를 작성하지 않고 공백으로 비워도 알아서 자동으로 프로퍼티명이 넘어가도록 하였음

---
+++ Assets/Scripts/Base/MVVM/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public abstract class BaseModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public abstract void PropertyChangedOnInit();
+}
