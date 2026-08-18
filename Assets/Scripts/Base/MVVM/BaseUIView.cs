2f793c254b5ef6628c7211b85eecabd687c46971
LeeKiWoong <littlemanner1@gmail.com>
2026-08-18 11:25:19 +0900

    [feat] BaseUIView<T> 추가 및 IUIView 인터페이스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### BaseUIView
    - BaseView<T>를 상속받는 UI전용 View 추상 클래스
    
    - Open을 통해서 게임 오브젝트를 활성화 하고, Close를 통해서 게임 오브젝트를 비활성화 하며, OnOpenAction 및 OnCloseAction을 protected virtual로 열어 UI를 열거나 닫을 때 추가 작업이 필요할 경우 오버라이딩해서 사용할 수 있도록 구현하였음
    
    #### IUIView
    - UI의 View가 가져야 할 열기와 닫기의 규약을 정의한 인터페이스로, Open과 Close 메서드를 가지고 있음

---
+++ Assets/Scripts/Base/MVVM/BaseUIView.cs
@@ -0,0 +1,24 @@
+﻿public abstract class BaseUIView<T> : BaseView<T>, IUIView where T : BaseViewModel
+{
+    public void Open()
+    {
+        this.ActiveTrue();
+        OnOpenAction();
+    }
+
+    public void Close()
+    {
+        OnCloseAction();
+        this.ActiveFalse();
+    }
+
+    protected virtual void OnOpenAction()
+    {
+        // UI를 열 때 필요한 추가작업이 있다면 여기에 작성
+    }
+
+    protected virtual void OnCloseAction()
+    {
+        // UI를 닫을 때 필요한 추가작업이 있다면 여기에 작성
+    }
+}
