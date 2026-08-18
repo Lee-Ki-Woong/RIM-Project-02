65f2dc930806cb0230770361b508082343a51d56
LeeKiWoong <littlemanner1@gmail.com>
2026-08-18 14:18:51 +0900

    [feat] GameDataBase 부모 클래스 및 SkinDataBase 클래스 추가
    ## 커밋 내용
    
    ### 추가
    
    #### GameDataBase
    - 모든 게임 내 데이터들의 부모 클래스로, string Id 필드를 보유하고 있음
    
    #### SkinDataBase
    - GameDataBase를 상속 받은 데이터 클래스로, 스킨의 카테고리, 이름, 설명, 아이콘 주소를 보유하고 있음

---
+++ Assets/Scripts/Data/GameDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[Serializable]
+public class GameDataBase
+{
+    public string Id;
+}
+
+[Serializable]
+public class SkinDataBase : GameDataBase
+{
+    public string SkinCategory;
+    public string SkinName;
+    public string SkinDescription;
+    public string SkinIconAddress;
+}
