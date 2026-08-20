3d12abca0789f5b463fdb5f967b0f5500bab58b7
LeeKiWoong <littlemanner1@gmail.com>
2026-08-20 11:32:57 +0900

    [feat] 임시로 구현되었던 GameManager 실제 구현
    ## 커밋 내용
    
    ### 수정
    
    #### GameManager
    - 매니저의 생성 및 현재 게임에 접속한 계정의 관리, 게임의 종료를 관리하는 BaseMonoManager를 상속받은 매니저 클래스
    
    - Awake 시점에 override된 AwakeAction 메서드를 통해 ResourceManager, NetworkManager, GameSettingManager, GameDataManager 등등의 여러 순수 C# 클래스들을 생성하고, SerializeField로 소지하고 있는 BaseMonoManager들을 Instantiate하도록 구현하였음
    
    - NetworkManager가 계정의 생성, 저장 및 로드를 담당한다면 GameManager는 그 데이터들을 받아와서 소지하고 있는 역할을 함 (나중에 서버로 바꿀 수 있기에 이렇게 구현하였음)
    
    - SetAccount로 로드된 Account를 세팅하고, SaveAccount로 현재 캐싱되어있는 GameCurrentAccountData를 저장하며, 게임을 정상적인 방법으로 종료할 때(QuitGame)도 SaveAccount를 호출하도록 구현하였음
    
    - 기존에 있던 GameCurrentLanguage 프로퍼티는 GameSettingManager로 이전하였음

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameManager : BaseMonoManager<GameManager>
 {
-    public Language GameCurrentLanguage { get; private set; }
+    [SerializeField] private GameObject Prefab_UIManager;
+
+    public AccountData GameCurrentAccountData { get; private set; }
+
+    protected override void AwakeAction()
+    {
+        ResourceManager.Create();
+        NetworkManager.Create();
+        GameSettingManager.Create();
+        GameDataManager.Create();
+        UIDataManager.Create();
+        SkinManager.Create();
+
+        Instantiate(Prefab_UIManager, this.transform);
+    }
+
+    public void SetAccount(AccountData account)
+    {
+        GameCurrentAccountData = account;
+    }
+
+    public void SaveAccount()
+    {
+        if(GameCurrentAccountData == null)
+        {
+            this.LogWarning("로그인한 계정이 없어 저장할 수 없습니다!!");
+            return;
+        }
+
+        NetworkManager.Instance.RequestSaveData(GameCurrentAccountData);
+    }
+
+    public void QuitGame()
+    {
+        SaveAccount();
+        Application.Quit();
+    }
 }
