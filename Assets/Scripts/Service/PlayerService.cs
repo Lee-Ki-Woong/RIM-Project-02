691a590a7b9308c9028ee9999fd783b85cf031d1
LeeKiWoong <littlemanner1@gmail.com>
2026-08-20 14:20:10 +0900

    [feat] PlayerModel을 관리하는 PlayerService 서비스로직 추가
    ## 커밋 내용
    
    ### 추가
    
    #### PlayerService
    - 로그인한 계정의 PlayerData를 관리하기 위해 만든 순수 C# 서비스 클래스로, GameManager가 관리하도록 설계하였음
    
    - 생성자에서 PlayerData를 받아 PlayerModel을 생성하며, Player의 Skin을 관리하기 위한 서비스 로직인 SkinService 또한 함께 생성하여 캐싱하도록 하였음
    
    - 런타임에서는 PlayerModel을 사용하며, 저장 시점에서 ApplyToData를 통하여 PlayerModel이 소지한 값을 다시 PlayerData로 옮기도록 구현하였음
    
    - 추후에 플레이어의 체력 증가/감소, 공격력 증가/감소 등등의 로직을 구현할 예정

---
+++ Assets/Scripts/Service/PlayerService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class PlayerService
+{
+    public PlayerModel Model { get; }
+    public SkinService SkinService { get; }
+
+    public PlayerService(PlayerData playerData)
+    {
+        Model = new(playerData);
+        SkinService = new(Model);
+    }
+
+    public void ApplyToData(PlayerData playerData)
+    {
+        playerData.PlayerName = Model.PlayerName;
+        playerData.PlayerMoveSpeed = Model.PlayerMoveSpeed;
+        playerData.PlayerAttackDamage = Model.PlayerAttackDamage;
+        playerData.PlayerMaxHp = Model.PlayerMaxHp;
+        playerData.PlayerCurrentHp = Model.PlayerCurrentHp;
+
+        playerData.OwnedSkins = new List<string>(Model.PlayerOwnedSkins);
+        playerData.SetEquippedSkins(Model.PlayerEquippedSkins);
+    }
+}
