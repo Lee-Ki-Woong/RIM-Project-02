422b226c576fd237bd886c1a8427f27179df9ffa
LeeKiWoong <littlemanner1@gmail.com>
2026-08-20 14:39:56 +0900

    [refactor] GameManager가 PlayerService를 캐싱하도록 수정
    ## 커밋 내용
    
    ### 수정
    
    #### GameManager
    - SetAccount로 로그인 계정 정보를 GameCurrentAccountData에 저장할 때, PlayerService를 생성하며 PlayerData를 주입하도록 변경
    
    - 생성한 PlayerService를 캐싱해두며, 계정 정보를 저장할 때(SaveAccount) 캐싱해둔 PlayerService의 ApplyToData메서드를 통해 PlayerModel의 데이터를 PlayerData에 덮어씌우도록 만들었음
    
    - SkinManager 대신 SkinService를 통해 관리하도록 개선하여 SkinManager는 삭제하였음

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : BaseMonoManager<GameManager>
 {
     [SerializeField] private GameObject Prefab_UIManager;
 
     public AccountData GameCurrentAccountData { get; private set; }
+    public PlayerService PlayerService { get; private set; }
 
     protected override void AwakeAction()
     {
@@ -14,7 +14,6 @@ public class GameManager : BaseMonoManager<GameManager>
         GameSettingManager.Create();
         GameDataManager.Create();
         UIDataManager.Create();
-        SkinManager.Create();
 
         Instantiate(Prefab_UIManager, this.transform);
     }
@@ -22,6 +21,7 @@ public class GameManager : BaseMonoManager<GameManager>
     public void SetAccount(AccountData account)
     {
         GameCurrentAccountData = account;
+        PlayerService = new(account.GameSaveData.PlayerData);
     }
 
     public void SaveAccount()
@@ -31,6 +31,8 @@ public class GameManager : BaseMonoManager<GameManager>
             this.LogWarning("로그인한 계정이 없어 저장할 수 없습니다!!");
             return;
         }
+
+        PlayerService.ApplyToData(GameCurrentAccountData.GameSaveData.PlayerData);
 
         NetworkManager.Instance.RequestSaveData(GameCurrentAccountData);
     }
