7df652d0a7d526019960a84e6eb24739a58ccc7c
LeeKiWoong <littlemanner1@gmail.com>
2026-08-20 14:27:36 +0900

    [feat] PlayerModel의 스킨 정보를 관리하는 SkinService 서비스로직 추가
    ## 커밋 내용
    
    ### 추가
    
    #### SkinService
    - 플레이어가 보유하거나 장착 중인 스킨의 조회 및 변경을 담당하는 서비스 로직으로 PlayerService가 생성할 때 만들어서 소지하도록 구현하였음
    
    - GetSkinCatalog(스킨 카탈로그 체크)는 GameDataManager가 불러온 스킨 카탈로그를 반환하도록 만들었으며, 나머지 GetEquippedSkins(플레이어가 장착중인 스킨 목록), GetOwnedSkins(플레이어가 소지한스킨 목록), IsOwned(플레이어의 스킨 소지 여부) 등은 _playerModel 필드를 확인해서 반환하도록 만들었음
    
    - SetEquippedSkins는 _playerModel의 장착 스킨을 파라미터에서 받아온 장착 스킨으로 변경하며, 그 후 GameManager를 통해서 계정에 저장하도록 구현하였음

---
+++ Assets/Scripts/Service/SkinService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SkinService
+{
+    private readonly PlayerModel _playerModel;
+
+    public SkinService(PlayerModel playerModel)
+    {
+        _playerModel = playerModel;
+    }
+
+    public IReadOnlyDictionary<SkinCategory, List<SkinDataBase>> GetSkinCatalog()
+    {
+        return GameDataManager.Instance.SkinData;
+    }
+
+    public IReadOnlyDictionary<SkinCategory, string> GetEquippedSkins()
+    {
+        return _playerModel.PlayerEquippedSkins;
+    }
+
+    public IReadOnlyCollection<string> GetOwnedSkins()
+    {
+        return _playerModel.PlayerOwnedSkins;
+    }
+
+    public bool IsOwned(string skinId)
+    {
+        return _playerModel.PlayerOwnedSkins.Contains(skinId);
+    }
+
+    public void SetEquippedSkins(IReadOnlyDictionary<SkinCategory, string> equippedSkins)
+    {
+        _playerModel.PlayerEquippedSkins = equippedSkins;
+        GameManager.Instance.SaveAccount();
+    }
+}
