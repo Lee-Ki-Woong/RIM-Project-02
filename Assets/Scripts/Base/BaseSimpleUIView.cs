55390ecc39e7ffce084ba6b8928046f8fa7f1c2e
LeeKiWoong <littlemanner1@gmail.com>
2026-08-24 18:36:19 +0900

    [feat] MVVM이 필요없는 UI를 위한 BaseSimpleUIView 추가
    ## 커밋 내용
    
    ### 추가
    
    #### BaseSimpleUIView
    - MVVM을 사용하지 않는 간단한 UI를 만들기 위한 View 클래스로 MonoBehaviour를 상속받으며 IUIView 인터페이스를 구현하였음
    
    - BaseUIView와 비슷하게 Open / Close에서 활성화/비활성화를 처리하며, 파생 클래스가 추가작업을 할 수 있도록 OnOpenAction 및 OnCloseAction 메서드를 protected virtual로 열어두었음

---
+++ Assets/Scripts/Base/BaseSimpleUIView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class BaseSimpleUIView : MonoBehaviour, IUIView
+{
+    public void Open()
+    {
+        this.ActiveTrue();
+        OnOpenAction();
+    }
+
+    public void Close()
+    {
+        OnCloseAction();
+        this.ActiveFalse();
+    }
+
+    protected virtual void OnOpenAction() { }
+    protected virtual void OnCloseAction() { }
+}
