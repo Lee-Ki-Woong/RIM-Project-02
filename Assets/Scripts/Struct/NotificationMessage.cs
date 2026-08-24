55752d094626ec6a336714aba165ee80f4e60232
LeeKiWoong <littlemanner1@gmail.com>
2026-08-24 20:12:32 +0900

    [feat] NotificationMessage 구조체 추가
    ## 커밋 내용
    
    ### 추가
    
    #### NotificationMessage
    - UI를 갱신할 때 NotificationType (에러, 성공) 정보와 텍스트를 한 번에 담는 구조체
    
    - UI 알림 정보와 텍스트를 한 번에 갱신하기 위해서 만듬
    
    - 알림이 없는 상태(초기화 상태)를 두기 위해서 Empty 정적 메서드를 통해 빈 구조체를 생성하게 만들어둠

---
+++ Assets/Scripts/Struct/NotificationMessage.cs
@@ -0,0 +1,18 @@
+﻿public readonly struct NotificationMessage
+{
+    public readonly string Text;
+    public readonly NotificationType Type;
+
+    public NotificationMessage(string text, NotificationType type)
+    {
+        Text = text;
+        Type = type;
+    }
+
+    public static NotificationMessage Empty()
+    {
+        NotificationMessage empty = new(string.Empty, NotificationType.None);
+
+        return empty;
+    }
+}
