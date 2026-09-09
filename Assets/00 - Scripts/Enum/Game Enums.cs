de398bcf1a2fa68a4b7d4b500113a1b38a187c46
LeeKiWoong <littlemanner1@gmail.com>
2026-09-09 19:51:19 +0900

    [refactor] 게임 및 UI 열거형과 구조체 이전
    ## 커밋 내용
    
    ### 추가
    
    #### Game Enums
    - 게임에서 필요한 열거형들을 한 cs 파일에 넣어두었음
    
    - Enum의 종류
    Language(언어)
    SkinCategory(스킨의 착용 부위)
    IdValidationResult(아이디 생성 시 성공 및 오류 정보)
    PasswordValidationResult(비밀번호 생성 시 성공 및 오류 정보)
    LoginResult(로그인 성공 및 실패 여부)
    CreateAccountResult(계정 생성 시 성공 및 오류 정보)
    PlayerAnimationState(Spine 애니메이션 종류)
    SkillCategory(스킬의 구분 정보(공격, 일반스킬, 궁극기))
    
    #### UI Enums
    - UI에서 필요한 열거형들을 한 cs파일에 넣어두었음
    
    - Enum의 종류
    UIType(UI의 종류)
    CanvasType(Canvas의 종류)
    GameOptionCategory(게임 옵션의 카테고리)
    NotificationType(알림의 종류, UI에서 알림의 종류로 표시 색상을 구분하는 데 사용 )

---
+++ Assets/00 - Scripts/Enum/Game Enums.cs
@@ -0,0 +1,66 @@
+﻿public enum Language
+{
+    Korean,
+    English,
+}
+
+public enum SkinCategory
+{
+    Hair,
+    Eyebrows,
+    Eyes,
+    Mouth,
+    Cloth,
+    Body,
+    Weapon,
+}
+
+public enum IdValidationResult
+{
+    Valid,
+    Empty,
+    TooShort,
+    TooLong,
+    InvalidFormat,
+}
+
+public enum PasswordValidationResult
+{
+    Valid,
+    Empty,
+    TooShort,
+    TooLong,
+    InvalidFormat,
+}
+
+public enum LoginResult
+{
+    Success,
+    Failed,
+}
+
+public enum CreateAccountResult
+{
+    Success,
+    InvalidId,
+    InvalidPassword,
+    PasswordMismatch,
+    AlreadyExists,
+}
+
+public enum PlayerAnimationState
+{
+    Idle_Relaxed,
+    Idle_Combat,
+    Walk_Relaxed,
+    Walk_Combat,
+    Skill,
+}
+
+public enum SkillCategory
+{
+    None,
+    Attack,
+    Skill,
+    Ultimate,
+}
---
+++ Assets/00 - Scripts/Enum/UI Enums.cs
@@ -0,0 +1,48 @@
+﻿public enum UIType
+{
+    None,
+
+    TitleBackgroundUI,
+    TitleGameOptionPopup,
+    TitleButtonUI,
+
+    LoginUI,
+
+    PlayerUID,
+
+    CharacterCreateUI,
+
+    MainMenuUI,
+    CharacterSkinPopup,
+
+    CharacterInfoUI,
+
+    GameOptionPopup,
+}
+
+public enum CanvasType
+{
+    None,
+    Background,
+    Main,
+    Content,
+    Popup_First,
+    Popup_Second,
+    Popup_Third,
+    Overlay,
+    Loading,
+}
+
+public enum GameOptionCategory
+{
+    Sound,
+    Video,
+    Language,
+}
+
+public enum NotificationType
+{
+    None,
+    Error,
+    Success,
+}
