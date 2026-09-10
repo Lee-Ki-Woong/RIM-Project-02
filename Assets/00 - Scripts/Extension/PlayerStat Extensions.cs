e12fc9caa7e9bfdcbb99b454437940863072b25b
LeeKiWoong <littlemanner1@gmail.com>
2026-09-10 13:23:21 +0900

    [refactor] 확장 메서드와 유틸리티 이전
    ## 커밋 내용
    
    ### 추가
    
    #### PlayerStat Extensions
    - PlayerLevelStatDataBase와 SkinDataBase를 추출하여 PlayerStat으로 변환하는 확장클래스
    
    - 레벨 데이터와 스킨 데이터가 같은 변환 로직을 가지고 있으므로, 로직을 각각의 서비스에서 하지 않도록 확장 메서드로 분리하였음 (=추후 로직이 변경되었을 때 한 cs 파일 내에서 하기위해 모아둠)
    
    ### 위치 변경
    
    #### Component Extensions, PlayerData Extensions, SkinCombiner, AccountUtil
    - 기존 코드들 이전 (변경사항 없음)
    
    ### 삭제
    
    #### MonoBehaviour Extensions
    - Component Extensions로 병합되며 빈 클래스가 되어 위치이동 할겸 삭제

---
+++ Assets/00 - Scripts/Extension/PlayerStat Extensions.cs
@@ -0,0 +1,32 @@
+﻿public static class PlayerStatExtensions
+{
+    public static PlayerStat ToPlayerStat(this PlayerLevelStatDataBase levelData)
+    {
+        PlayerStat playerStat = new();
+
+        playerStat.MaxHp = levelData.MaxHp;
+        playerStat.AttackDamage = levelData.AttackDamage;
+        playerStat.AttackSpeed = levelData.AttackSpeed;
+        playerStat.Armour = levelData.Armour;
+        playerStat.CritRate = levelData.CritRate;
+        playerStat.CritDamage = levelData.CritDamage;
+        playerStat.MoveSpeed = levelData.MoveSpeed;
+
+        return playerStat;
+    }
+
+    public static PlayerStat ToPlayerStat(this SkinDataBase skinData)
+    {
+        PlayerStat playerStat = new();
+
+        playerStat.MaxHp = skinData.MaxHp;
+        playerStat.AttackDamage = skinData.AttackDamage;
+        playerStat.AttackSpeed = skinData.AttackSpeed;
+        playerStat.Armour = skinData.Armour;
+        playerStat.CritRate = skinData.CritRate;
+        playerStat.CritDamage = skinData.CritDamage;
+        playerStat.MoveSpeed = skinData.MoveSpeed;
+
+        return playerStat;
+    }
+}
