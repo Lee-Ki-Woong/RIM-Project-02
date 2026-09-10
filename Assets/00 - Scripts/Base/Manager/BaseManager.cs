49c99dea4d18b367af84cac4b4394055b78e4166
LeeKiWoong <littlemanner1@gmail.com>
2026-09-10 13:57:59 +0900

    [refactor] 매니저와 서비스의 Base 클래스 이전
    ## 커밋 내용
    
    ### 위치 변경
    
    #### BaseManager, BaseMonoManager, BaseService
    - 기존 코드들 이전 (변경사항 없음)

---
+++ Assets/00 - Scripts/Base/Manager/BaseManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public abstract class BaseManager<T> where T : BaseManager<T>, new()
+{
+    public static T Instance { get; private set; }
+
+    protected BaseManager()
+    {
+        InitSingleTon();
+        InitAction();
+    }
+
+    public static void Create()
+    {
+        if (Instance != null)
+        {
+            return;
+        }
+
+        new T(); // 생성자를 이 메서드에서 호출하여 Instance가 생성됨
+    }
+
+    private void InitSingleTon()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+        }
+        else
+        {
+            throw new System.Exception($"이미 존재하고 있는 싱글톤 인스턴스 입니다. {typeof(T)}");
+        }
+    }
+
+    protected virtual void InitAction()
+    {
+        // Initialize 할 때 필요한 추가 작업이 있다면 여기에 작성
+    }
+
+    protected void Log(string text)
+    {
+        Debug.Log($"{this} : " + text);
+    }
+
+    protected void LogWarning(string text)
+    {
+        Debug.LogWarning($"{this} : " + text);
+    }
+
+    protected void LogError(string text)
+    {
+        Debug.LogError($"{this} : " + text);
+    }
+}
---
+++ Assets/00 - Scripts/Base/Manager/BaseMonoManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public abstract class BaseMonoManager<T> : MonoBehaviour where T : BaseMonoManager<T>
+{
+    public static T Instance { get; private set; }
+
+    private void Awake()
+    {
+        InitSingleTon();
+        AwakeAction();
+    }
+
+    private void OnDestroy()
+    {
+        OnDestroyAction();
+
+        if(ReferenceEquals(Instance, this))
+        {
+            Instance = null;
+        }
+    }
+
+    private void InitSingleTon()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+
+            if (transform.parent == null)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void AwakeAction()
+    {
+        // Awake에서 추가 작업이 필요할 경우 여기에 작성
+    }
+    
+    protected virtual void OnDestroyAction()
+    {
+        // OnDestroy에서 추가 작업이 필요할 경우 여기에 작성
+    }
+}
